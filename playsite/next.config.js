d3d59a6e46f0e9c92b58800666d2780bf93a7a42
Arul Pragasam <arulpragasam2003@gmail.com>
2026-04-04 10:06:25 +0530

    next config

--- playsite/next.config.js
+++ playsite/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  experimental: {
-    outputFileTracingRoot: undefined,
-  },
 };
 module.exports = nextConfig;
